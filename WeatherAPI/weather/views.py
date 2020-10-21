ddb6db9e8393bb1be17402d9e8228825ceac2a44
Facundo Palavecino <facundo6991@gmail.com>
2020-10-21 17:50:09 -0300

    Requests are cached for 2 minutes

--- WeatherAPI/weather/views.py
+++ WeatherAPI/weather/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-
+from django.core.cache import cache
+from datetime import datetime
 
 @api_view(['GET'])
 def weather_view(request, format=None):
@@ -11,7 +12,8 @@ def weather_view(request, format=None):
 
     params = {
         'city': request.query_params.get('city', None),
-        'country': request.query_params.get('country', None)
+        'country': request.query_params.get('country', None),
+        'requested_time': str(datetime.now())
     }
 
     for k, v in params.items():
@@ -21,4 +23,16 @@ def weather_view(request, format=None):
     if len(params['country']) != 2:
         return Response(data=f"'country' value's length must be 2", status=status.HTTP_400_BAD_REQUEST)
 
-    return Response(data=f"I'm working! Params received: {str(params)}", status=status.HTTP_200_OK)
+    params['city'] = params.get('city').title()
+    params['country'] = params.get('country').lower()
+
+    weather = cache.get(f"{params.get('city')}_{params.get('country')}")
+
+    if weather is not None:
+        print(f"Weather has been retrieved from cache: {str(weather)}")
+        return Response(data=f"{weather}", status=status.HTTP_200_OK)
+    else:
+        # Perform get to external API
+        cache.set(f"{params.get('city')}_{params.get('country')}", str(params), 60*2)
+        print(f"A new value has been cached for 2 mintues: {str(params)}")
+    return Response(data=f"{str(params)}", status=status.HTTP_200_OK)
